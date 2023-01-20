979f9a9aec2db0fc3d7a8f94629df1c3a95b6b64
gellowg <47644702+gellowg@users.noreply.github.com>
2023-01-20 12:46:20 +0000

    Add files via upload

---
+++ CT14/CT14_Activity_4_Student.py
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------
+# Global variables
+# ------------------------------------------------------------
+myList = [150, 153, 163, 198, 200, 210, 212, 217, 251,
+          263, 281, 311, 318, 365, 376, 401, 411, 441,
+          583, 616, 620, 636, 716, 802, 807, 807, 840,
+          841, 877, 896]
+
+# ------------------------------------------------------------
+# Subprograms
+# ------------------------------------------------------------
+def reverseSearch (inList, inTarget):
+    index = 0
+    found = False
+    passed = False
+
+# -----> Add the code to search the list, only looping until
+#        the item is found or the place where it should
+#        be located at is passed
+
+
+    if (found):
+        print (inTarget, "found at index", index)
+    else:
+        print (inTarget, "not found")
+
+# ------------------------------------------------------------
+# Main program
+# ------------------------------------------------------------
+reverseSearch (myList, 636)
+reverseSearch (myList, 150)
+reverseSearch (myList, 130)
+
+reverseSearch (myList, 896)
+reverseSearch (myList, 720)
+reverseSearch (myList, 900)
