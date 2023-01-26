5f5145c1df0001493cd2b0aeb485e65be617544a
George Tong <47644702+gellowg@users.noreply.github.com>
2023-01-26 10:57:59 +0000

    f

---
+++ CT16/CT16_Activity_4_Finished.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------------------
+# Global variables
+# ------------------------------------------------------------
+nouns = ["cakes", "bicycles", "trees", "pigeons", "houses"]
+verbs = ["eat", "ride", "hug", "chase", "draw"]
+sentence = ""
+userInput = "N"
+
+# ------------------------------------------------------------
+# Main program
+# ------------------------------------------------------------
+while (userInput == "N"):
+    nounIndex = int(input("Enter a noun index: "))
+    verbIndex = int(input("Enter a verb index: "))
+
+    sentence = "The children " + verbs[verbIndex] + " " + nouns[nounIndex] + "."
+    print(sentence)
+
+    userInput = input("Does this sentence make sense (Y/N)? ")
+    userInput = userInput.upper()
+
+print("Goodbye")
